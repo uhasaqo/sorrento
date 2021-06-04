cb172fe54a5d0f3bcefb2fa53378258c7f5acebd
uhasaqo <jcott2019@gmail.com>
2021-06-04 18:12:29 +1000

    June 4th Commit

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,7 +1,7 @@
 import { Banner } from './Banner'
 import BannerImage from '../banners/nao-takabayashi-uM9EEuc69g0-unsplash (1).jpg'
 import '../styles/Pricing.css'
-import {Link} from 'react-router-dom' 
+import { Link } from 'react-router-dom'
 
 export function Pricing(props) {
     return (
@@ -15,7 +15,7 @@ export function Pricing(props) {
                     <h2>Winter</h2>
                     <h4>1st June - August 31st</h4>
                     <ul>
-                    
+
                         <li>$200 per night</li>
                         <li>The pricing is for 2 people.</li>
                     </ul>
@@ -39,17 +39,20 @@ export function Pricing(props) {
                         <li>A full refund will be given prior to 30 days cancellation</li>
                         <li>50% refund will be given after 30 days of cancellation</li>
                         <li>In the event of a weekly or monthly booking the apartment will be serviced weekly unless by
-                    prior arrangement.</li><p></p>
-                    
-                <Link to="/contact" className="button">Book now</Link>
+                    prior arrangement.</li></ul><p></p>
+                    <h2>Act now!</h2>
+                        <Link to="/contact" className="button">Book your stay</Link>
+                        <p></p>
+                        <p></p>
+                        
 
-                    </ul>
                     
+
                 </div>
             </div>
 
-            
-            
+
+
 
         </div>
 
