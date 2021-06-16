274c3add20fee76795bee4f27388cb27e8ddff65
uhasaqo <jcott2019@gmail.com>
2021-06-16 16:50:37 +1000

    final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export function Footer(props) {
     <footer className="main-footer">
 
       <p className="copyright"><b> &copy; 2021 Sorrento by the Sea</b><br></br><i>(website, apartment photos)</i></p>
-      <p className="address-footer"><b><a href="tel:0435 123 456">0435 123 456</a> / 42 Holyrood Avenue, Sorrento, Victoria 3943</b></p>
+      <p className="address-footer"><b><a href="tel:0435 123 456" label="Click to call us!">0435 123 456</a> / 42 Holyrood Avenue, Sorrento, Victoria 3943</b></p>
 
       <nav className="social-nav">
         <a href="https://facebook.com/SorrentobytheSea">
